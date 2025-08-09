58974073ecd9ea89c8f5e33590d9d1d630dc92d9
Paramita Parinyanupap <61963531+paramita-pp@users.noreply.github.com>
2025-08-09 22:46:43 +0700

    Update and rename placeholder to silver_quality_test.sql

---
+++ tests/silver_quality_test.sql
@@ -0,0 +1,17 @@
+/*
+====================================================================================
+Quality Checks
+====================================================================================
+Script Purpose:
+  This script performs quality checks to validate the integrity, consitency,
+  and accuracy of the Gold Layer. These checks ensure:
+  - Uniqueness of surrogate keys in dimension tables
+  - Referential integrity between fact and dimension tables
+  - Validation of relationships in the data model for analytical purposes
+
+Usage Notes:
+  - Run those checks after data loading Silver Layer.
+  - Investigate and resolve any discrepancies found during the checks.
+====================================================================================
+
+*/
